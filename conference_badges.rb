3eb5fda7f8f230046145536eea517cdb9a59b605
Fausto Ramirez <ramirezmedia@gmail.com>
2023-08-05 19:44:15 -0400

    commit tree 11e58dabf6bdf1c88432374efcba40c8158d97e2

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+def badge_maker(name)
+    sentence = "Hello, my name is #{name}."
+    #puts(sentence)
+    return sentence
+end
+
+
+def batch_badge_creator(names)
+    messages = [];
+
+    messages = names.map do |name|
+        badge_maker(name)
+    end
+    messages
+end
+
+def assign_rooms(speakers)
+    assignment = [];
+
+    assignment = speakers.each_with_index.map do |speaker, index|
+        "Hello, #{speaker}! You'll be assigned to room #{index + 1}!"
+    end
+
+    assignment
+end
+
+def printer(names)
+    greeting = batch_badge_creator(names)
+
+    rooms = assign_rooms(names)
+
+    greeting.map do |guest|
+        puts guest
+    end
+
+    rooms.map do |speak|
+        puts speak
+    end
+end
